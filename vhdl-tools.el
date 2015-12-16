775db63d62d70f0bdd28eaf7e0042d64c1fbbdfb
Cayetano Santos <cayetano.santos@apc.univ-paris7.fr>
2015-12-16 16:47:46 +0100

    Fixed bug in jump upper

--- vhdl-tools.el
+++ vhdl-tools.el
@@ -365,7 +365,7 @@ When no symbol at point, move point to indentation."
       (funcall `(lambda ()
 		  (minibuffer-with-setup-hook
 		      (lambda ()
-			(insert (format ".*: *entity.*%s$" ,(vhdl-tools-get-name))))
+			(insert (format "^.* : \\(entity work.\\)*%s$" ,(vhdl-tools-get-name))))
 		    (helm-grep-do-git-grep t))))
       ;; search, when nil, do nothing
       (when vhdl-tools-thing
