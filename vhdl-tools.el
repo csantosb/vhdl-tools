07db24ac29698a6393936421dcc0707e1ab8185c
Cayetano Santos <csantosb@inventati.org>
2016-08-08 17:47:20 +0200

    Bump to 5.1

--- vhdl-tools.el
+++ vhdl-tools.el
@@ -14,7 +14,7 @@
 ;; URL: https://github.com/csantosb/vhdl-tools
 ;; Keywords: convenience
 ;; Compatibility: GNU Emacs >= 24.5
-;; Version: 5.0
+;; Version: 5.1
 ;; Package-Requires: ((ggtags "0.8.11") (emacs "24.5") (outshine "2.0") (helm "1.9.9"))
 
 ;;; License:
