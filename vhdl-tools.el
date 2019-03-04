f3a97d5c3ff0bd6be8a794aa501958c3cb6b588a
Cayetano Santos <csantosb@inventati.org>
2019-03-04 22:23:26 +0100

    Outshine is no more a dependency

--- vhdl-tools.el
+++ vhdl-tools.el
@@ -13,7 +13,7 @@
 ;; URL: https://github.com/csantosb/vhdl-tools/wiki
 ;; Compatibility: GNU Emacs >= 25.2
 ;; Version: 5.9
-;; Package-Requires: ((ggtags "0.8.12") (emacs "26.1") (outshine "2.0") (helm "2.9.6") (helm-rg "0.1"))
+;; Package-Requires: ((ggtags "0.8.12") (emacs "26.1") (helm "2.9.6") (helm-rg "0.1"))
 
 ;;; License:
 ;;
